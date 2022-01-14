59cedc949a15f5bd4f7912ee9b861eac8b283e8c
Nikaidou-Shinku <1106609918@qq.com>
2022-01-14 14:46:14 +0800

    fix: auto update

---
+++ public/Full.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         洛谷用户卡片~
+// @namespace    http://tampermonkey.net/
+// @icon         https://cdn.luogu.com.cn/upload/usericon/3.png
+// @author       yurzhang & tiger2005
+// @homepage     https://github.com/Nikaidou-Shinku/Luogu-usercard
+// @description  A plugin to decorate Luogu with exquisite user card.
+// @updateURL    https://raw.githubusercontent.com/Nikaidou-Shinku/Luogu-usercard/master/public/Full.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Nikaidou-Shinku/Luogu-usercard/master/public/Full.user.js
+// @include      https://www.luogu.com.cn/*
+// @version      0.2.2
+// @grant        none
+// @license      MIT
+// ==/UserScript==
