e0d601ec6c1969b4ab522710529e4ef6bf4afca7
tiger2005 <41613797+tiger2005@users.noreply.github.com>
2022-01-21 17:23:40 +0800

    Add link regex

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,10 +4,11 @@ export const userPageRegex = [
   /^space\/show\?uid=\d+$/,
   /^\/space\/show\?uid=\d+$/,
   /^https:\/\/www\.luogu\.com\.cn\/user\/\d+$/,
-  /^https:\/\/www\.luogu\.com\.cn\/space\/show\?uid=\d+$/
+  /^https:\/\/www\.luogu\.com\.cn\/space\/show\?uid=\d+$/,
+  /^https:\/\/www\.luogu\.org\/space\/show\?uid=\d+$/
 ];
 
-export const userPageUrlIndex = [ 5, 6, 15, 16, 30, 40 ];
+export const userPageUrlIndex = [ 5, 6, 15, 16, 30, 40, 37 ];
 
 export module consts {
   export let currentUID: number = -1;
