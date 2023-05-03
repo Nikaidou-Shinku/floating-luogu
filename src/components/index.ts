7eb65f54100c8ad370e2121a7c5030e2288accc7
yurzhang <yurzhang@163.com>
2023-05-03 21:51:26 +0800

    refa: all

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Hello } from "./Hello";
-export { CardLoader } from "./CardLoader";
-export { Card } from "./Card";
-export { InfoCard } from "./InfoCard";
-export { FailedCard } from "./FailedCard";
-export { LoadingCard } from "./LoadingCard";
-export { loadAddress } from "./AddressLoader";
